4647795e5544e0e61f29d69d471bb6d7d1c18b22
Ondrej Marek <xmarek9@fi.muni.cz>
2024-07-25 12:26:27 +0200

    feat: add video duration to the DTO

--- YoutubeDownloader/DTOs/VideoDataDto.cs
+++ YoutubeDownloader/DTOs/VideoDataDto.cs
@@ -2,4 +2,4 @@ using YoutubeExplode.Common;
 
 namespace YoutubeDownloader.DTOs;
 
-public record VideoDataDto(string Url, string Author, string Title, string ThumbnailUrl);
+public record VideoDataDto(string Url, string Author, string Title, string ThumbnailUrl, TimeSpan Duration);
