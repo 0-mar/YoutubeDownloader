13b98dbce95151b65ca366044b40ef3bd711098e
Ondrej Marek <xmarek9@fi.muni.cz>
2024-08-03 10:58:27 +0200

    feat: download audio now has default directory parameter

--- YoutubeDownloader/Services/IYoutubeService.cs
+++ YoutubeDownloader/Services/IYoutubeService.cs
@@ -7,7 +7,7 @@ public interface IYoutubeService
 {
     public Task<VideoDataBatchDto> GetVideoInfo(string searchQuery, string nextPageToken);
 
-    public Task<string> DownloadAudio(string url);
+    public Task<string> DownloadAudio(string url, string directory="");
 
     public Task<PlaylistDataDto> GetPlaylistInfo(string url);
 
