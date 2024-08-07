bc5f95a6a19762e2da02e92ad364fa6a46dbdbfe
Ondrej Marek <xmarek9@fi.muni.cz>
2024-08-07 14:29:07 +0200

    feat: send type parameter

--- YoutubeDownloader/wwwroot/js/downloadPage/index.js
+++ YoutubeDownloader/wwwroot/js/downloadPage/index.js
@@ -2,10 +2,12 @@ import Utils from "../utils.js";
 
 async function downloadAudio(){
     const url = document.getElementById('video-url').getAttribute('href');
+    const type = document.getElementById('type-data').value;
     
     try {
         await Utils.downloadData("/Download/GetResource", {
-            url: url
+            url: url,
+            type: type
         });
     } catch (error) {
         console.error('There was a problem with the fetch operation:', error);
@@ -15,48 +17,3 @@ async function downloadAudio(){
 }
 
 downloadAudio();
-
-
-
-
-/*const url = "/Download/GetResource?" + new URLSearchParams({
-    url: "@Html.Raw(Model.Url)"
-}).toString();
-
-const headers = {
-    "Content-type": "application/json"
-};
-
-fetch(url, {
-    method: "GET",
-    headers: headers
-})
-    .then(response => {
-        if (!response.ok) {
-            document.getElementById("toast--error").hidden = false;
-            return;
-        }
-
-        response.blob().then((blob) => {
-            const downloadUrl = URL.createObjectURL(blob);
-            const contentDisposition = response.headers.get("content-disposition");
-            let fileName = "audio.mp3";
-            const filenameRegex = /filename="([^"]+)"/;
-            const match = filenameRegex.exec(contentDisposition);
-            if (match && match[1]) {
-                fileName = match[1];
-            }
-            console.log(fileName);
-            const a = document.createElement('a');
-            a.style.display = 'none';
-            a.href = downloadUrl;
-            a.download = fileName;
-            document.body.appendChild(a);
-            a.click();
-            URL.revokeObjectURL(downloadUrl);
-        });
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        document.getElementById("toast--error").hidden = false;
-    });*/
