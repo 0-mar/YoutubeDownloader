90bec85633602836d498ca7babfaedbb7bad86bd
Ondrej Marek <xmarek9@fi.muni.cz>
2024-08-07 14:27:31 +0200

    feat: add support for the download type choice

--- YoutubeDownloader/Controllers/DownloadController.cs
+++ YoutubeDownloader/Controllers/DownloadController.cs
@@ -10,6 +10,16 @@ namespace YoutubeDownloader.Controllers
     {
 
         private readonly IYoutubeService _youtubeService;
+        private static readonly Dictionary<string, string> ContentTypes = new()
+        {
+            {"audio", "audio/mpeg"},
+            {"video", "video/mpeg"},
+        };
+        private static readonly Dictionary<string, string> Extensions = new()
+        {
+            {"audio", "mp3"},
+            {"video", "webm"},
+        };
 
         public DownloadController(IYoutubeService youtubeService)
         {
@@ -17,27 +27,28 @@ namespace YoutubeDownloader.Controllers
         }
         
         [HttpGet]
-        public IActionResult Index(string url)
+        public IActionResult Index(string url, string type)
         {
-            var urlModel = new UrlModel(url);
-            return View(urlModel);
+            var downloadModel = new DownloadModel(url, type);
+            return View(downloadModel);
 
         }
         
         [HttpGet]
-        public async Task<IActionResult> GetResource(string url)
+        public async Task<IActionResult> GetResource(string url, string type)
         {
             var filePath = "";
+            var contentType = ContentTypes[type];
+            var ext = Extensions[type];
             
             try
             {
-                filePath = await _youtubeService.DownloadAudio(url);
+                filePath = await _youtubeService.DownloadAudio(url, extension: ext);
                 
                 // when I put 'using' in front, it gives me System.ObjectDisposedException: Cannot access a closed file.
                 FileStream fileStream = new FileStream(filePath, FileMode.Open,FileAccess.Read);
                 
-                // Return the file to the client
-                return File(fileStream, "audio/mpeg", filePath);
+                return File(fileStream, contentType, filePath);
             }
             catch (ArgumentException ex)
             {
