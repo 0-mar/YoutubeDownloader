1baf1e3d5d1b72944ff3c34fb855cc7db35adf50
Ondrej Marek <xmarek9@fi.muni.cz>
2024-09-01 21:41:51 +0200

    feat: add user model

---
+++ YoutubeDownloader/Models/Database/User.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace YoutubeDownloader.Models.Database;
+
+public class User : IdentityUser
+{
+    public DateTime CreatedOn { get; set; }
+
+    public string ProfilePicSource { get; set; }
+}
