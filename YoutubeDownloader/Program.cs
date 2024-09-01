c55e93698187bc6a37fca98871cc2ae6857ff614
Ondrej Marek <xmarek9@fi.muni.cz>
2024-09-01 22:57:16 +0200

    feat: add database support, seeding and services for identity

--- YoutubeDownloader/Program.cs
+++ YoutubeDownloader/Program.cs
@@ -1,14 +1,53 @@
-using YoutubeDownloader;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.EntityFrameworkCore;
+using YoutubeDownloader.Database;
+using YoutubeDownloader.Identity;
+using YoutubeDownloader.Models.Database;
+using YoutubeDownloader.Seeding;
 using YoutubeDownloader.Services;
 
 DotNetEnv.Env.Load();
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING")));
+
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IYoutubeService, YoutubeService>();
+builder.Services.AddScoped<Seeder>();
+builder.Services.AddScoped<IUserClaimsPrincipalFactory<User>, AppUserClaimsPrincipalFactory>();
+builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+// auth
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+    {
+        options.SignIn.RequireConfirmedEmail = true;
+    })
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.SlidingExpiration = true;
+    options.ExpireTimeSpan = TimeSpan.FromMinutes(30);
+});
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
+
+// seeding
+using (var scope = app.Services.CreateScope())  
+{  
+    var seeder = scope.ServiceProvider.GetRequiredService<Seeder>();
+
+    await seeder.SeedAsync();
+} 
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -23,6 +62,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
