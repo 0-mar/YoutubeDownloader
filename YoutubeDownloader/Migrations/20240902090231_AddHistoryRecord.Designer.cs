670d0f7f27e3d29e04092d2aa63f042172b15895
Ondrej Marek <xmarek9@fi.muni.cz>
2024-09-02 11:05:30 +0200

    feat: add history record migration

--- YoutubeDownloader/Migrations/20240902090231_AddHistoryRecord.Designer.cs
+++ YoutubeDownloader/Migrations/20240902090231_AddHistoryRecord.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using YoutubeDownloader.Database;
@@ -11,9 +12,11 @@ using YoutubeDownloader.Database;
 namespace YoutubeDownloader.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240902090231_AddHistoryRecord")]
+    partial class AddHistoryRecord
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ YoutubeDownloader/Migrations/20240902090231_AddHistoryRecord.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace YoutubeDownloader.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHistoryRecord : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "HistoryRecords",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DownloadedOn = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    VideoTitle = table.Column<string>(type: "text", nullable: false),
+                    VideoUrl = table.Column<string>(type: "text", nullable: false),
+                    ThumbnailUrl = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HistoryRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_HistoryRecords_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HistoryRecords_UserId",
+                table: "HistoryRecords",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HistoryRecords");
+        }
+    }
+}
