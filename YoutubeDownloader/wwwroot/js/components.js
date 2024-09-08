48330994f320616db269e36e68de102a2d4df7f9
Ondrej Marek <xmarek9@fi.muni.cz>
2024-09-08 14:18:17 +0200

    feat: add history record card and loading components

--- YoutubeDownloader/wwwroot/js/components.js
+++ YoutubeDownloader/wwwroot/js/components.js
@@ -15,7 +15,7 @@ const Components = (function () {
         url.href = videoData.url;
         url.textContent = videoData.url;
         const duration = document.createElement('p');
-        author.textContent = videoData.duration;
+        duration.textContent = videoData.duration;
 
         container.appendChild(title);
         container.appendChild(author);
@@ -62,9 +62,67 @@ const Components = (function () {
         return card;
     }
     
+    const historyRecordCard = function (historyRecordData) {
+        const container = document.createElement('div');
+        container.className = "video-card";
+
+        const date = document.createElement('p');
+        date.textContent = Utils.convertDateFromIso(historyRecordData.downloadedOn);
+
+        const title = document.createElement('h3');
+        title.textContent = historyRecordData.videoTitle;
+
+        const url = document.createElement('a');
+        url.href = historyRecordData.videoUrl;
+        url.textContent = historyRecordData.videoUrl;
+
+        const thumbnail = document.createElement("img");
+        thumbnail.src = historyRecordData.thumbnailUrl;
+
+        const downloadAgainButton = document.createElement('button');
+        downloadAgainButton.className = "video-download-btn";
+        downloadAgainButton.textContent = 'Download again';
+        downloadAgainButton.addEventListener('click', () => Utils.redirect(`Download?url=${videoData.url}&type=audio`));
+
+        container.appendChild(date);
+        container.appendChild(title);
+        container.appendChild(url);
+        container.appendChild(thumbnail);
+        container.appendChild(downloadAgainButton);
+        
+        return container
+    }
+    
+    const loadingSkeleton = function () {
+        const container = document.createElement('div');
+        container.className = "loading-skeleton";
+
+        const text = document.createElement('p');
+        text.textContent = "Loading...";
+        
+        container.appendChild(text);
+        
+        return container;
+    }
+
+    const noMoreDataCard = function () {
+        const container = document.createElement('div');
+        container.className = "no-more-data-card";
+
+        const text = document.createElement('p');
+        text.textContent = "No more data to load";
+
+        container.appendChild(text);
+        
+        return container;
+    }
+    
     return {
         downloadVideoCard,
-        playlistCard
+        playlistCard,
+        historyRecordCard,
+        loadingSkeleton,
+        noMoreDataCard
     }
 })();
 
