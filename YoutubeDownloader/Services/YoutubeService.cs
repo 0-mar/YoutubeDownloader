6063179a08c14f0bb319a313f9cdb79f61bb83d1
Ondrej Marek <xmarek9@fi.muni.cz>
2024-09-08 14:14:22 +0200

    feat: refactor video data fetching, every download logs to the user history

--- YoutubeDownloader/Services/YoutubeService.cs
+++ YoutubeDownloader/Services/YoutubeService.cs
@@ -1,7 +1,10 @@
 using System.Xml;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.WebUtilities;
 using Newtonsoft.Json;
+using YoutubeDownloader.Database;
 using YoutubeDownloader.DTOs;
+using YoutubeDownloader.Models.Database;
 using YoutubeExplode;
 using YoutubeExplode.Common;
 using YoutubeExplode.Converter;
@@ -14,21 +17,29 @@ namespace YoutubeDownloader.Services;
 public class YoutubeService : IYoutubeService
 {
     private readonly HttpClient _httpClient;
-    private YoutubeClient Youtube;
+    private readonly YoutubeClient _youtube;
+    private readonly AppDbContext _context;
+    private readonly UserManager<User> _userManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public YoutubeService(HttpClient httpClient)
+    public YoutubeService(HttpClient httpClient, AppDbContext context, 
+        UserManager<User> userManager,IHttpContextAccessor httpContextAccessor)
     {
         _httpClient = httpClient;
-        Youtube = new YoutubeClient();
+        _context = context;
+        _userManager = userManager;
+        _httpContextAccessor = httpContextAccessor;
+        
+        _youtube = new YoutubeClient();
     }
 
-    private async Task<List<TimeSpan>> GetVideosDuration(IEnumerable<string> videoIds)
+    public async Task<List<VideoDataDto>> GetVideosData(IEnumerable<string> videoIds)
     {
         string baseUrl = "https://www.googleapis.com/youtube/v3/videos";
         var parameters = new Dictionary<string, string>
         {
             { "id", String.Join(",", videoIds) },
-            { "part", "contentDetails" },
+            { "part", "snippet,contentDetails" },
             { "key", Environment.GetEnvironmentVariable("YOUTUBE_API_KEY") },
         };
 
@@ -39,19 +50,26 @@ public class YoutubeService : IYoutubeService
         var response = await _httpClient.SendAsync(request);
         var stringResponse = await response.Content.ReadAsStringAsync();
 
+        List<VideoDataDto> result = new List<VideoDataDto>();
+        
         VideoRoot? root = JsonConvert.DeserializeObject<VideoRoot>(stringResponse);
-        List<TimeSpan> durations = new List<TimeSpan>();
-
         foreach (var video in root.items)
         {
             TimeSpan parsedDuration = XmlConvert.ToTimeSpan(video.contentDetails.duration);
-            durations.Add(parsedDuration);
+            string videoUrl = $"https://www.youtube.com/watch?v={video.id}";
+            var title = video.snippet.title;
+            var authorName = video.snippet.channelTitle;
+            var thumbnailsObj = video.snippet.thumbnails;
+            var thumbnailUrl = thumbnailsObj.high != null ? thumbnailsObj.high.url :
+                thumbnailsObj.medium != null ? thumbnailsObj.medium.url : thumbnailsObj.@default.url;
+            
+            result.Add(new VideoDataDto(videoUrl, authorName, title, thumbnailUrl, parsedDuration));
         }
 
-        return durations;
+        return result;
     }
 
-    public async Task<VideoDataBatchDto> GetVideoInfo(string searchQuery, string nextPageToken)
+    public async Task<VideoDataBatchDto> GetVideoSearchData(string searchQuery, string nextPageToken)
     {
         string url = "https://www.googleapis.com/youtube/v3/search";
         var parameters = new Dictionary<string, string>
@@ -60,7 +78,7 @@ public class YoutubeService : IYoutubeService
             { "part", "snippet" },
             { "type", "video" },
             { "key", Environment.GetEnvironmentVariable("YOUTUBE_API_KEY") },
-            { "maxResults", "15" }
+            { "maxResults", "10" }
         };
         if (nextPageToken != "")
         {
@@ -81,20 +99,7 @@ public class YoutubeService : IYoutubeService
         }
 
         IEnumerable<string> videoIds = root.items.Select(item => item.id.videoId);
-        List<TimeSpan> videoDurations = await GetVideosDuration(videoIds);
-
-        List<VideoDataDto> videoDataDtos = new List<VideoDataDto>();
-        foreach ((var item, var duration) in root.items.Zip(videoDurations, (i1, i2) => (i1, i2)))
-        {
-            var videoUrl = $"https://www.youtube.com/watch?v={item.id.videoId}";
-            var title = item.snippet.title;
-            var authorName = item.snippet.channelTitle;
-            var thumbnailsObj = item.snippet.thumbnails;
-            var thumbnailUrl = thumbnailsObj.high != null ? thumbnailsObj.high.url :
-                thumbnailsObj.medium != null ? thumbnailsObj.medium.url : thumbnailsObj.@default.url;
-
-            videoDataDtos.Add(new VideoDataDto(videoUrl, authorName, title, thumbnailUrl, duration));
-        }
+        List<VideoDataDto> videoDataDtos = await GetVideosData(videoIds);
 
         return new VideoDataBatchDto(root.nextPageToken, videoDataDtos);
     }
@@ -102,25 +107,42 @@ public class YoutubeService : IYoutubeService
     public async Task<string> DownloadAudio(string url, string directory="", string extension="mp3")
     {
         var filePath = "";
-
-        /*var streamManifest = await Youtube.Videos.Streams.GetManifestAsync(url);
-        var streamInfo = streamManifest.GetAudioOnlyStreams().GetWithHighestBitrate();
-        var video = await Youtube.Videos.GetAsync(url);*/
-        //filePath = Path.Combine(directory, $"{video.Title}.{streamInfo.Container.Name}");
-        //filePath = Path.Combine(directory, $"{video.Title}.mp3");
-
-        //await Youtube.Videos.Streams.DownloadAsync(streamInfo, filePath);
         
-        var video = await Youtube.Videos.GetAsync(url);
+        var video = await _youtube.Videos.GetAsync(url);
         filePath = Path.Combine(directory, $"{video.Title}.{extension}");
-        await Youtube.Videos.DownloadAsync(url, filePath);
+        await _youtube.Videos.DownloadAsync(url, filePath);
 
+        await CreateHistoryRecord(url);
+        
         return filePath;
+    }
+
+    private async Task CreateHistoryRecord(string url)
+    {
+        var startIdx = url.IndexOf("v=") + 2;
+        var endIdx = url.IndexOf('&', startIdx);
+
+        var videoId = url.Substring(startIdx, endIdx - startIdx);
+                
+        var data = (await GetVideosData(new []{videoId}))[0];
+        var user = await _userManager.GetUserAsync(_httpContextAccessor.HttpContext?.User);
+                
+        _context.HistoryRecords.Add(new HistoryRecord
+        {
+            DownloadedOn = DateTime.UtcNow,
+            Id = new Guid(),
+            User = user,
+            ThumbnailUrl = data.ThumbnailUrl,
+            UserId = user.Id,
+            VideoTitle = data.Title,
+            VideoUrl = data.Url
+        });
+        await _context.SaveChangesAsync();
     }
 
     public async Task<PlaylistDataDto> GetPlaylistInfo(string url)
     {
-        var playlist = await Youtube.Playlists.GetAsync(url);
+        var playlist = await _youtube.Playlists.GetAsync(url);
 
         var playlistTitle = playlist.Title;
         var playlistAuthor = "Author not available";
@@ -132,7 +154,7 @@ public class YoutubeService : IYoutubeService
 
         List<VideoDataDto> videoData = new List<VideoDataDto>();
 
-        await foreach (var video in Youtube.Playlists.GetVideosAsync(url))
+        await foreach (var video in _youtube.Playlists.GetVideosAsync(url))
         {
             var title = video.Title;
             var author = video.Author.ChannelTitle;
