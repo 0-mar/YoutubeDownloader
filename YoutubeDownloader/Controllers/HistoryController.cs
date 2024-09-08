b9cd531419c28fcd04ccff3b69e2a9493b58580d
Ondrej Marek <xmarek9@fi.muni.cz>
2024-09-08 14:18:55 +0200

    feat: add history controller

---
+++ YoutubeDownloader/Controllers/HistoryController.cs
@@ -0,0 +1,58 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using YoutubeDownloader.Database;
+using YoutubeDownloader.DTOs;
+
+namespace YoutubeDownloader.Controllers
+{
+    
+    [Authorize(Roles = "Regular")]
+    public class HistoryController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HistoryController(AppDbContext context)
+        {
+            _context = context;
+        }
+        
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<HistoryRecordsPageDto> GetHistoryRecords(Guid? lastId = null, DateTime? lastDownloadDate = null)
+        {
+            if (lastId == null && lastDownloadDate == null)
+            {
+                var firstPage = await _context.HistoryRecords
+                    .OrderBy(b => b.DownloadedOn)
+                    .ThenBy(b => b.Id)
+                    .Take(10)
+                    .ToListAsync();
+
+                return new HistoryRecordsPageDto
+                {
+                    Page = firstPage,
+                    Length = firstPage.Count
+                };
+            }
+
+            var nextPage = await _context.HistoryRecords
+                .OrderBy(b => b.DownloadedOn)
+                .ThenBy(b => b.Id)
+                .Where(b => b.DownloadedOn > lastDownloadDate || (b.DownloadedOn == lastDownloadDate && b.Id > lastId))
+                .Take(10)
+                .ToListAsync();
+
+            return new HistoryRecordsPageDto
+            {
+                Page = nextPage,
+                Length = nextPage.Count
+            };
+        }
+    }
+}
