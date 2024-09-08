91261f8d099ea767f459deb5d1a2ae89b8bb61ff
Ondrej Marek <xmarek9@fi.muni.cz>
2024-09-08 14:11:51 +0200

    feat: introduce new method to get video info data from collection of video ids

--- YoutubeDownloader/Services/IYoutubeService.cs
+++ YoutubeDownloader/Services/IYoutubeService.cs
@@ -5,7 +5,8 @@ namespace YoutubeDownloader.Services;
 
 public interface IYoutubeService
 {
-    public Task<VideoDataBatchDto> GetVideoInfo(string searchQuery, string nextPageToken);
+    public Task<List<VideoDataDto>> GetVideosData(IEnumerable<string> videoIds);
+    public Task<VideoDataBatchDto> GetVideoSearchData(string searchQuery, string nextPageToken);
 
     public Task<string> DownloadAudio(string url, string directory="", string extension="mp3");
 
